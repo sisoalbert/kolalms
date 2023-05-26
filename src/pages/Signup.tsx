8b838c6470b4b8568c33cd5326a8a5ef85312edc
sisoalbert <sisoalbert@yahoo.com>
2023-05-26 06:28:44 +0200

    added google sign up

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -12,8 +12,38 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log("email", email);
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // navigate("/login");
+        console.log("success");
+
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+  };
+
   return (
     <Flex
       minH={"100vh"}
@@ -23,7 +53,7 @@ function Signup() {
     >
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Heading fontSize={"4xl"}>Sign up for an account</Heading>
           <Text fontSize={"lg"} color={"gray.600"}>
             to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
           </Text>
@@ -37,11 +67,25 @@ function Signup() {
           <Stack spacing={4}>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input
+                type="email"
+                // label="Email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder="Email address"
+              />
             </FormControl>
             <FormControl id="password">
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
+              <Input
+                type="password"
+                // label="Create password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder="Password"
+              />
             </FormControl>
             <Stack spacing={10}>
               <Stack
@@ -53,17 +97,21 @@ function Signup() {
                 <Link color={"blue.400"}>Forgot password?</Link>
               </Stack>
               <Button
+                onClick={onSubmit}
                 bg={"blue.400"}
                 color={"white"}
                 _hover={{
                   bg: "blue.500",
                 }}
               >
-                Sign in
+                Sign up
               </Button>
             </Stack>
           </Stack>
         </Box>
+        <p>
+          Already have an account? <NavLink to="/login">Sign in</NavLink>
+        </p>
       </Stack>
     </Flex>
   );
